5da6be294d59c6219c5b9f982abd026296faecf1
Vishal5977 <vishalp5977@gmail.com>
2026-08-02 09:36:24 +0530

    Delete list_codes.py

--- list_codes.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv(r'C:\Users\Asus\embroidery-finder\design_database.csv')
-codes = sorted(df['design_name'].unique())
-print("Total unique designs indexed:", len(codes))
-print("Design codes:", codes)
